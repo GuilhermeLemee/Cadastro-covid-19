4a31f9c3ab81d39de990d15e21c52142520422f2
GuilhermeLemee <58531227+GuilhermeLemee@users.noreply.github.com>
2021-11-27 23:07:38 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+struct atendimento
+{
+	char nome[50];
+	char cpf[15];
+	int fone[15];
+	char endereco[100];
+	char cep[10];
+	char nascimento[10];
+	int anoNasc;
+	char email[50];
+	char datadiag[10];
+	char comorbidade[30];
+};
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	struct atendimento paciente;
+	FILE *arquivo;
+	int idade;
+
+	char login[10] = "admin", log[10];
+	int senha = 123, sen;
+
+	do{
+		printf("Digite seu login: \n");
+		scanf("%s", &log);
+		fflush(stdin);
+		system("cls");
+
+		if(strcmp(log, login) == 1){
+			printf("Login incorreto. Digite novamente.\n");
+		}
+	}while(strcmp(log, login) == 1);
+
+	do{
+		printf("Digite a senha: \n");
+		scanf("%d", &sen);
+		fflush(stdin);
+		system("cls");
+
+		if(sen != senha){
+			printf("Senha incorreta. Digite novamente");
+		}
+	}while(sen != senha);
+
+	arquivo = fopen("dados.txt", "a");
+	if(arquivo == NULL)
+	{
+		printf("O arquivo não foi aberto. Ocorreu um erro!\n");
+	}
+	else
+	{
+
+
+	printf("Cadastro Inicial do Paciente\n");
+
+	printf("\nNome: \n");
+	gets(paciente.nome);
+	fflush(stdin);
+
+	printf("CPF: \n");
+	gets(paciente.cpf);
+	fflush(stdin);
+
+	printf("Telefone: \n");
+	scanf("%d", &paciente.fone);
+	fflush(stdin);
+
+	printf("Endereço: \n");
+	gets(paciente.endereco);
+	fflush(stdin);
+
+	printf("Informe o CEP: \n");
+	gets(paciente.cep);
+	fflush(stdin);
+
+	printf("Informe dia e mês de nascimento: \n");
+	gets(paciente.nascimento);
+	fflush(stdin);
+
+	printf("Informe o ano de nascimento: \n");
+	scanf("%d", &paciente.anoNasc);
+	fflush(stdin);
+
+	printf("E-mail: \n");
+	gets(paciente.email);
+	fflush(stdin);
+
+	printf("Dia do diagnostico: \n");
+	gets(paciente.datadiag);
+	fflush(stdin);
+
+	printf("Comorbidade:\nCaso paciente não apresente comorbidade, deixar esse campo em branco\n");
+	gets(paciente.comorbidade);
+	fflush(stdin);
+	system("cls");
+
+
+	idade = 2021 - paciente.anoNasc;
+
+	if((idade > 65) || (strcmp(paciente.comorbidade, " ")) == 1){
+		printf("Paciente em grupo de risco\n");
+		printf("Dados pessoais salvos em dados.txt");
+
+		fprintf(arquivo, "\n-------------------------------------------\n");
+		fprintf(arquivo, "Nome: %s\n", paciente.nome);
+		fprintf(arquivo, "Idade: %d\n", idade);
+		fprintf(arquivo, "CEP: %s\n", paciente.cep);
+		fprintf(arquivo, "Comorbidade: %s\n", paciente.comorbidade);
+		fprintf(arquivo, "-------------------------------------------\n");
+	}else{
+		printf("Fora do grupo de risco\n");
+	}
+}
+	fclose(arquivo);
+}
+
